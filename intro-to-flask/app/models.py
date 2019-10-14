9d3afae232bad21ae960b3b38a6dab3e3c044599
qhobeey <bludohric@gmail.com>
2019-10-14 16:36:00 +0000

    login and registration form validation and logic

--- intro-to-flask/app/models.py
+++ intro-to-flask/app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
@@ -19,6 +20,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
 
 
--- intro-to-flask/config.py
+++ intro-to-flask/config.py
@@ -2,4 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY',  'you-will-never-guess')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
